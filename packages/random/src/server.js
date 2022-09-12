dedf75afb6838036761223e23439a15769a0d80d
vanpho93 <vanpho02@gmail.com>
2022-09-12 14:44:49 +0700

    feat: add console log to seed

--- packages/random/src/server.js
+++ packages/random/src/server.js
@@ -5,6 +5,7 @@ import RandomGenerator from "./RandomGenerator";
 const Random = new RandomGenerator({ nodeCrypto });
 
 Random.createWithSeeds = (...seeds) => {
+  console.log(seeds)
   if (seeds.length === 0) throw new Error("No seeds were provided");
   return new RandomGenerator({ seeds });
 };
